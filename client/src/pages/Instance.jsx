0e538ca239db8bdeaf5490c8bde8ac30cbbb8887
Richie Arnold R <157049939+richieArnrich@users.noreply.github.com>
2025-03-01 01:13:36 +0530

    Update Instance.jsx

--- client/src/pages/Instance.jsx
+++ client/src/pages/Instance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Instance = axios.create({
-  baseURL: "http://localhost", // Set the base URL
+  baseURL: "https://book-recommend-app-api.vercel.app/", // Set the base URL
   headers: {
     "Content-Type": "application/json",
   },
